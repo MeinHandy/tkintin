a1211cb2c1bb0e10f213b9e4e269c4a133a88c27
peanutchad <cmiles45365346@gmail.com>
2023-03-10 23:01:27 +1300

    inventory and shop purchasing works with functional currency, inventory to be relocated and selling to shop to be implemented

--- main.py
+++ main.py
@@ -1,20 +1,25 @@
 from tkinter import *
 from tkinter import ttk
 
-# Create a variable to store the account balance
+# variables to store the account balance
 savings_balance = 0
 phone_balance = 0
 holiday_balance = 0
 enemy_stats = []
-player_inventory = []
+player_inventory = ["old sock"]
 player_balance = 0
 player_stats = {"Level": 1, "Health": 20, "Damage": 5}
+wares = ["short sword", "mace", "broadsword"]  # shop items can be added freely, dictionary for individual prices?
 
 
+# all the subroutines
 def menu():
     clear_bottom_frame()
-    action_box.grid(row=4, column=1, padx=10, pady=3, sticky="WE")
-    submit_button.grid(row=6, column=0, columnspan=2, padx=10, pady=10)
+    action_box.grid(row=4, column=0, padx=10, pady=3, sticky="WE")
+    submit_button.grid(row=5, column=0, padx=10, pady=10)
+    balance_label.grid(row=3, column=0)  # row, column subject to change
+    balance_display.grid(row=3, column=1)
+    inventory_box.grid(row=4, column=1)
 
 
 def clear_bottom_frame():
@@ -29,9 +34,10 @@ def combat():  # called on button press for combat
 
 
 def fought():  # for after combat
-    global player_balance
+    global player_balance, balance_total
     clear_bottom_frame()
-    player_balance += 2
+    player_balance += 5  # money gained post combat, temporary probably
+    balance_total.set(player_balance)  # sets the DoubleVar to balance to update GUI
     combat_over.grid(row=4, column=1, columnspan=2, padx=10, pady=10)
     menu_button.grid(row=6, column=0, columnspan=2, padx=10, pady=10)
 
@@ -50,11 +56,12 @@ def shop():  # called on button press
 
 
 def purchase_item():
-    global player_balance, player_inventory
+    global player_balance, player_inventory, balance_total, inventory_display
     if player_balance >= 10:
         player_balance -= 10
+        balance_total.set(player_balance)
         print("{} bought".format(shop_item.get()))
-        player_inventory.append(shop_item)
+        get_item()
     else:
         print("Insufficient funds")
 
@@ -69,7 +76,13 @@ def intersection():
     elif choice == "Shop":
         shop()
     else:
-        print("drug deal")
+        print("the funny secret else statement you cannot achieve.. unless you can. pls report bug")
+
+
+def get_item():
+    global player_inventory
+    player_inventory.append(shop_item.get())
+    inventory_box['values'] = player_inventory
 
 
 # tkinter init
@@ -80,7 +93,7 @@ top_frame = ttk.LabelFrame(root, text="Player")
 top_frame.grid(row=0, column=0, padx=10, pady=10, sticky="NSEW")
 
 player_info = StringVar()
-player_info.set(player_stats)
+player_info.set(player_stats)  # lol w/e
 
 details_label = ttk.Label(top_frame, textvariable=player_info)
 details_label.grid(row=2, column=0, columnspan=2, padx=10, pady=10)
@@ -118,12 +131,25 @@ submit_button = ttk.Button(bottom_frame, text="Continue", command=intersection)
 menu_button = ttk.Button(bottom_frame, text="Return", command=menu)
 
 # shop
-wares = ["short sword", "mace", "broadsword"]
 shop_item = StringVar()
 shop_item.set(wares[0])
 shop_combobox = ttk.Combobox(bottom_frame, textvariable=shop_item, state="readonly")
 shop_combobox['values'] = wares
-purchase_button = ttk.Button(bottom_frame, text="Buy", command=lambda: purchase_item())
+purchase_button = ttk.Button(bottom_frame, text="Buy", command=lambda: purchase_item())  # lambda is useless here but im
+# keeping it to remember how to do things. pycharm 120-character limit moment
+
+# money update real time
+balance_label = ttk.Label(top_frame, text="Money: ")
+balance_total = DoubleVar()
+balance_total.set(0)
+balance_display = Label(top_frame, textvariable=balance_total)
+
+# inventory, as combobox for now, may have other tab later, copied shop code, basically
+inventory_display = StringVar()
+inventory_display.set(player_inventory[0])
+inventory_box = ttk.Combobox(top_frame, textvariable=inventory_display, state="readonly")
+inventory_box['values'] = player_inventory
+
 
 # Run the mainloop and starts menu
 menu()
