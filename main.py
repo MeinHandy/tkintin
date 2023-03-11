71a2069a11ca416ebf5a8e6b1803c893984489f1
peanutchad <cmiles45365346@gmail.com>
2023-03-11 14:58:32 +1300

    inventory better placement, message displays bought item in GUI rather than in pycharm

--- main.py
+++ main.py
@@ -9,21 +9,22 @@ enemy_stats = []
 player_inventory = ["old sock"]
 player_balance = 0
 player_stats = {"Level": 1, "Health": 20, "Damage": 5}
-wares = ["short sword", "mace", "broadsword"]  # shop items can be added freely, dictionary for individual prices?
+wares = ["short sword", "mace", "broadsword", "the throngler"]
+# shop items can be added freely, dictionary for individual prices?
 
 
-# all the subroutines
+# all the function
 def menu():
     clear_bottom_frame()
     action_box.grid(row=4, column=0, padx=10, pady=3, sticky="WE")
     submit_button.grid(row=5, column=0, padx=10, pady=10)
     balance_label.grid(row=3, column=0)  # row, column subject to change
     balance_display.grid(row=3, column=1)
-    inventory_box.grid(row=4, column=1)
+    inventory_box.grid(row=1, column=0)
 
 
 def clear_bottom_frame():
-    for widget in bottom_frame.winfo_children():
+    for widget in action_frame.winfo_children():
         widget.grid_forget()
 
 
@@ -50,9 +51,9 @@ def sleep():  # called on button press
 
 def shop():  # called on button press
     clear_bottom_frame()
-    shop_combobox.grid(row=4, column=1, columnspan=2, padx=10, pady=10)
-    menu_button.grid(row=6, column=0, columnspan=2, padx=10, pady=10)
-    purchase_button.grid(row=6, column=2, columnspan=2, padx=10, pady=10)
+    shop_combobox.grid(row=1, column=0, padx=10, pady=10)
+    menu_button.grid(row=2, column=0, padx=10, pady=10)
+    purchase_button.grid(row=2, column=1, padx=10, pady=10)
 
 
 def purchase_item():
@@ -60,10 +61,12 @@ def purchase_item():
     if player_balance >= 10:
         player_balance -= 10
         balance_total.set(player_balance)
-        print("{} bought".format(shop_item.get()))
+        bought_item_string.set(shop_item.get() + " bought")
+        bought_item.grid(row=1, column=1)
         get_item()
     else:
-        print("Insufficient funds")
+        bought_item_string.set("Insufficient funds")
+        bought_item.grid(row=1, column=1)
 
 
 # runs function for each choice when chosen
@@ -89,65 +92,69 @@ def get_item():
 root = Tk()
 root.title("Shit game")
 # menu widgets
-top_frame = ttk.LabelFrame(root, text="Player")
-top_frame.grid(row=0, column=0, padx=10, pady=10, sticky="NSEW")
+player_frame = ttk.LabelFrame(root, text="Player")
+player_frame.grid(row=0, column=0, padx=10, pady=10, sticky="NSEW")
 
 player_info = StringVar()
 player_info.set(player_stats)  # lol w/e
 
-details_label = ttk.Label(top_frame, textvariable=player_info)
+details_label = ttk.Label(player_frame, textvariable=player_info)
 details_label.grid(row=2, column=0, columnspan=2, padx=10, pady=10)
 
-bottom_frame = ttk.LabelFrame(root)
-bottom_frame.grid(row=1, column=0, padx=10, pady=10, sticky="NSEW")
+action_frame = ttk.LabelFrame(root)
+action_frame.grid(row=1, column=0, padx=10, pady=10, sticky="NSEW")
 
-action_label = ttk.Label(bottom_frame, text="Action:")
-action_label.grid(row=4, column=0)
+# action_label = ttk.Label(action_frame, text="Action:")
+# action_label.grid(row=4, column=0)
 
 action_list = ["Fight", "Sleep", "Shop"]
 chosen_action = StringVar()
 chosen_action.set(action_list[0])
 
-action_box = ttk.Combobox(bottom_frame, textvariable=chosen_action, state="readonly")
+action_box = ttk.Combobox(action_frame, textvariable=chosen_action, state="readonly")
 action_box['values'] = action_list
 
 # combat var ect
 enemy = StringVar()
 enemy.set("Boneman")
-enemy_label = ttk.Label(bottom_frame, textvariable=enemy)
+enemy_label = ttk.Label(action_frame, textvariable=enemy)
 post_combat = StringVar()
 post_combat.set("Battle ended.")
-combat_over = ttk.Label(bottom_frame, textvariable=post_combat)
+combat_over = ttk.Label(action_frame, textvariable=post_combat)
 
 # rest var ect
 resting = StringVar()
 resting.set("You feel rested.")
-rest_label = ttk.Label(bottom_frame, textvariable=resting)
+rest_label = ttk.Label(action_frame, textvariable=resting)
 
 # submit send to intersection function
-attack_button = ttk.Button(bottom_frame, text="Attack", command=fought)
-submit_button = ttk.Button(bottom_frame, text="Continue", command=intersection)
+attack_button = ttk.Button(action_frame, text="Attack", command=fought)
+submit_button = ttk.Button(action_frame, text="Continue", command=intersection)
 # general menu button
-menu_button = ttk.Button(bottom_frame, text="Return", command=menu)
+menu_button = ttk.Button(action_frame, text="Return", command=menu)
 
 # shop
 shop_item = StringVar()
 shop_item.set(wares[0])
-shop_combobox = ttk.Combobox(bottom_frame, textvariable=shop_item, state="readonly")
+shop_combobox = ttk.Combobox(action_frame, textvariable=shop_item, state="readonly")
 shop_combobox['values'] = wares
-purchase_button = ttk.Button(bottom_frame, text="Buy", command=lambda: purchase_item())  # lambda is useless here but im
-# keeping it to remember how to do things. pycharm 120-character limit moment
+purchase_button = ttk.Button(action_frame, text="Buy", command=purchase_item)
+bought_item_string = StringVar()
+bought_item_string.set("placeholder")
+bought_item = ttk.Label(action_frame, textvariable=bought_item_string)
 
 # money update real time
-balance_label = ttk.Label(top_frame, text="Money: ")
+balance_label = ttk.Label(player_frame, text="Money: ")
 balance_total = DoubleVar()
 balance_total.set(0)
-balance_display = Label(top_frame, textvariable=balance_total)
+balance_display = Label(player_frame, textvariable=balance_total)
 
 # inventory, as combobox for now, may have other tab later, copied shop code, basically
+inventory_frame = ttk.LabelFrame(root, text="Inventory")  # frame for the inventory, inventory will be expanded sometime
+inventory_frame.grid(row=0, column=1, padx=10, pady=10, sticky="NSEW")
 inventory_display = StringVar()
 inventory_display.set(player_inventory[0])
-inventory_box = ttk.Combobox(top_frame, textvariable=inventory_display, state="readonly")
+inventory_box = ttk.Combobox(inventory_frame, textvariable=inventory_display, state="readonly")
 inventory_box['values'] = player_inventory
 
 
