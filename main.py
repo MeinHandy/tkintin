9b30290e02b6515433ee34f9668db02dc21421ff
ah19017 <ah19017@melville-high.school.nz>
2023-03-13 09:37:12 +1300

    everything is rearranged and functional (probably)

--- main.py
+++ main.py
@@ -6,24 +6,27 @@ enemy_stats = []
 player_inventory = ["old sock"]
 player_balance = 0
 player_stats = {"Level": 1, "Health": 20, "Damage": 5}
-wares = {"short sword": 20, "mace": 20, "broadsword": 30, "the throngler": 45, "Health potion": 10}
+wares = {"Short sword": 20, "Mace": 20, "Broadsword": 30, "The Throngler": 45, "Health potion": 10}
 consumable_items = ["Health potion", "old sock", "moist old sock", "soggy old sock", "damp old sock", "wet old sock"]
 # list of what can be consumed shop items can be added freely, dictionary allows for unique prices
 
 
 # all the function
-def menu():
-    clear_bottom_frame()
+def menu():  # this resets GUI
+    clear_shop_frame()
     clear_combat_frame()
     consumed_label.grid_forget()
-    balance_label.grid(row=3, column=0)  # row, column subject to change
+    balance_label.grid(row=3, column=0)
     balance_display.grid(row=3, column=1)
     inventory_box.grid(row=1, column=0)
     start_combat.grid()
+    shop_combobox.grid(row=1, column=0, padx=10, pady=10)
+    purchase_button.grid(row=2, column=1, padx=10, pady=10)
+    rest_button.grid()
 
 
-def clear_bottom_frame():
-    for widget in action_frame.winfo_children():
+def clear_shop_frame():
+    for widget in shop_frame.winfo_children():
         widget.grid_forget()
 
 
@@ -32,16 +35,22 @@ def clear_combat_frame():
         widget.grid_forget()
 
 
+def clear_rest_frame():
+    for widget in rest_frame.winfo_children():
+        widget.grid_forget()
+
+
 def init_combat():  # called on button press for combat
     global player_balance, balance_total
+    clear_shop_frame()
     clear_combat_frame()
+    clear_rest_frame()
     enemy_label.grid(row=4, column=1, columnspan=2, padx=20, pady=10)
     attack_button.grid(row=6, column=0, columnspan=2, padx=10, pady=10)
 
 
 def fought():  # for after combat
     global player_balance, balance_total
-    clear_bottom_frame()
     player_stats["Health"] -= 5
     update_stats()  # player loses health, will be accurate damage at some point maybe
     if player_stats["Health"] > 0:
@@ -65,19 +74,6 @@ def respawn():  # resets stats after respawn button is pressed
     inventory_box['values'] = player_inventory  # re-displays inventory so its empty
     inventory_display.set(player_inventory[0])  # makes the selected item the first, so it doesn't show old item
     menu()  # sends player to the menu
-
-
-def sleep():  # called on button press
-    clear_bottom_frame()
-    rest_label.grid(row=4, column=1, columnspan=2, padx=10, pady=10)
-    menu_button.grid(row=6, column=0, columnspan=2, padx=10, pady=10)
-
-
-def shop():  # called on button press
-    clear_bottom_frame()
-    shop_combobox.grid(row=1, column=0, padx=10, pady=10)
-    menu_button.grid(row=2, column=0, padx=10, pady=10)
-    purchase_button.grid(row=2, column=1, padx=10, pady=10)
 
 
 def purchase_item():
@@ -123,8 +119,7 @@ def use_item():  # consume item logic
 
         consumed_text.set(inventory_box.get() + " consumed.")
         consumed_label.grid()
-        if inventory_box.get() != "drenched old sock":
-            player_inventory.remove(inventory_box.get())  # removes it from the inventory
+        player_inventory.remove(inventory_box.get())  # removes it from the inventory
         inventory_box['values'] = player_inventory  # updates the inventory combobox
         inventory_display.set(player_inventory[0])  # changes back to item 0, so it no longer displays the item
     else:
@@ -143,6 +138,10 @@ def update_stats():
                                                    list(player_stats)[2], player_stats['Damage']))
 
 
+def resting():
+    print("we rest")
+
+
 # tkinter init
 root = Tk()
 root.title("Sock Simulator 2023")
@@ -154,27 +153,15 @@ update_stats()
 details_label = ttk.Label(player_frame, textvariable=player_info)
 details_label.grid(row=2, column=0, columnspan=2, padx=10, pady=10)
 
-action_frame = ttk.LabelFrame(root)
-action_frame.grid(row=1, column=0, padx=10, pady=10, sticky="NSEW")
-
-# action_label = ttk.Label(action_frame, text="Action:")
-# action_label.grid(row=4, column=0)
-
-action_list = ["Fight", "Sleep", "Shop"]
-chosen_action = StringVar()
-chosen_action.set(action_list[0])
-
 # combat var ect
-combat_frame = ttk.LabelFrame(root)
+combat_frame = ttk.LabelFrame(root, text="Dungeon")
 combat_frame.grid(row=1, column=1, sticky="NSEW", padx=10, pady=10)
 start_combat = ttk.Button(combat_frame, text="Fight", command=init_combat)
 start_combat.grid()
 enemy = StringVar()
 enemy.set("Boneman")
 enemy_label = ttk.Label(combat_frame, textvariable=enemy)
-post_combat = StringVar()
-post_combat.set("Battle ended.")
-combat_over = ttk.Label(combat_frame, textvariable=post_combat)
+combat_over = ttk.Label(combat_frame, text="Battle ended.")
 # death during combat
 death_label = ttk.Label(combat_frame, text="You have perished.")
 respawn_button = ttk.Button(combat_frame, text="Respawn", command=respawn)
@@ -182,9 +169,11 @@ respawn_button = ttk.Button(combat_frame, text="Respawn", command=respawn)
 attack_enemy = ttk.Button(root, text="Attack Enemy!")
 
 # rest var ect
-resting = StringVar()
-resting.set("You feel rested.")
-rest_label = ttk.Label(action_frame, textvariable=resting)
+rest_frame = ttk.LabelFrame(root, text="Home")
+rest_frame.grid(row=3, column=0, sticky="NSEW", padx=10, pady=10)
+rest_label = ttk.Label(rest_frame, text="You feel rested.")
+rest_button = ttk.Button(rest_frame, text="Rest", command=resting)
+rest_button.grid()
 
 # submit send to intersection function
 attack_button = ttk.Button(combat_frame, text="Attack", command=fought)
@@ -192,14 +181,17 @@ attack_button = ttk.Button(combat_frame, text="Attack", command=fought)
 menu_button = ttk.Button(combat_frame, text="Return", command=menu)
 
 # shop
+
+shop_frame = ttk.LabelFrame(root, text="Shop")
+shop_frame.grid(row=1, column=0, padx=10, pady=10, sticky="NSEW")
 shop_item = StringVar()
 shop_item.set("Health potion")
-shop_combobox = ttk.Combobox(action_frame, textvariable=shop_item, state="readonly")
+shop_combobox = ttk.Combobox(shop_frame, textvariable=shop_item, state="readonly")
 shop_combobox['values'] = list(wares)
-purchase_button = ttk.Button(action_frame, text="Buy", command=purchase_item)
+purchase_button = ttk.Button(shop_frame, text="Buy", command=purchase_item)
 bought_item_string = StringVar()
 bought_item_string.set("funny secret message")
-bought_item = ttk.Label(action_frame, textvariable=bought_item_string)
+bought_item = ttk.Label(shop_frame, textvariable=bought_item_string)
 
 # money update real time
 balance_label = ttk.Label(player_frame, text="Money: ")
